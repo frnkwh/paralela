6a9fe6a54cbd77a5d07adfe7c3520c466214bbf8
CAIO HENRIQUE RAMOS RUFINO <chrr22@inf.ufpr.br>
2024-12-12 23:31:11 -0300

    Adicionando os scripts

---
+++ t3/rodar-exp1-slurm-1n-8ppn.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --exclusive
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --job-name=multi-partition-mpi
+echo -----------------------------------
+echo -----------------------------------
+echo -----------------------------------
+echo ---------- ATENCAO: usar --exclusive na linha de comando
+echo -----------------------------------
+
+
+OUTPUT_FILE="saida_exp1-${SLURM_JOBID}.txt"
+
+##### SBATCH --output=slurm_exp1-%j.out
+
+echo "$0 rodando no host " `hostname` 
+echo "$0 rodando no host " `hostname` > "$OUTPUT_FILE"
+
+echo "SLURM_JOB_NAME: "	$SLURM_JOB_NAME	
+echo "SLURM_NODELIST: " $SLURM_NODELIST 
+echo "SLURM_JOB_NODELIST: " $SLURM_JOB_NODELIST
+echo "SLURM_JOB_CPUS_PER_NODE: " $SLURM_JOB_CPUS_PER_NODE
+
+# Load necessary modules (adjust paths as needed)
+#module load openmpi/4.1.4
+
+# Run the MPI program
+mpirun ./multi-partition-mpi 8000000 8 \
+| tee -a "$OUTPUT_FILE" | grep -oP '(?<=total_time_in_seconds: )[^ ]*'
+
+
+echo "O tempo total dessa shell foi de" $SECONDS "segundos"
+echo "SLURM_JOB_NAME: "	$SLURM_JOB_NAME	
+echo "SLURM_NODELIST: " $SLURM_NODELIST 
+echo "SLURM_JOB_NODELIST: " $SLURM_JOB_NODELIST
+echo "SLURM_JOB_CPUS_PER_NODE: " $SLURM_JOB_CPUS_PER_NODE
+#imprime infos do job slurm (e.g. TEMPO até aqui no fim do job)
+squeue -j $SLURM_JOBID
